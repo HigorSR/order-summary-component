b0e33da814eee5474dbb02862bdce84e77b00e51
Higor Rocha <higor_36rocha@hotmail.com>
2023-12-03 03:36:58 -0300

    Created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "Pale-blue": "hsl(225, 100%, 94%)",
+        "Bright-blue": "hsl(245, 75%, 52%)",
+
+        "Very-pale-blue": "hsl(225, 100%, 98%)",
+        "Desaturated-blue": "hsl(224, 23%, 55%)",
+        "Dark-blue": "hsl(223, 47%, 23%)"
+      }
     },
   },
   plugins: [],
